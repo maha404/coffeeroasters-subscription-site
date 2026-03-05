2f0769a715b7fc8b438010904037eba30b59bb0b
maha404 <maria_halvarsson@hotmail.se>
2026-03-05 20:47:05 +0100

    Add responsive images to Header component and adjust styles

--- coffee-subscription-site/src/Components/Header.tsx
+++ coffee-subscription-site/src/Components/Header.tsx
@@ -1,5 +1,7 @@
 
 import mobileHero from '../assets/home/mobile/image-hero-coffeepress.jpg'
+import tabletHero from '../assets/home/tablet/image-hero-coffeepress.jpg'
+import desktopHero from '../assets/home/desktop/image-hero-coffeepress.jpg'
 import NavigationMobile from './NavigationMobile'
 import Button from './Button'
 
@@ -8,7 +10,11 @@ export default function Header() {
         <header>
             <NavigationMobile />
             <section className="header-section">
-            <img src={mobileHero} alt="" />
+            <picture>
+                <source media="(min-width: 768px)" srcSet={tabletHero} />
+                <source media="(min-width: 1440px)" srcSet={desktopHero} />
+                <img src={mobileHero} alt="" />
+            </picture>
             <div className="header-content">
                 <h1>Great coffee made simple.</h1>
                 <p>
