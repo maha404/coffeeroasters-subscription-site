4a36044f4033f76ea34a3237d3ff895db91f6cad
maha404 <maria_halvarsson@hotmail.se>
2026-03-07 21:47:15 +0100

    Add HowItWorksSection component with StepCard integration and styling updates

--- coffee-subscription-site/src/App.tsx
+++ coffee-subscription-site/src/App.tsx
@@ -3,17 +3,19 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import CollectionSection from './Components/CollectionSection';
 import BenefitSection from './Components/BenefitSection';
+import HowItWorksSection from './Components/HowItWorksSection';
 
 function App() {
 
   return (
-    <>
+    <div className='main-container'>
       <Header />
       {/* <Navigation /> */}
       <CollectionSection  />
       <BenefitSection />
+      <HowItWorksSection />
       <Footer />
-    </>
+    </div>
   )
 }
 
--- coffee-subscription-site/src/Components/StepCard.tsx
+++ coffee-subscription-site/src/Components/StepCard.tsx
@@ -7,7 +7,7 @@ interface props {
 
 export default function StepCard(props : props) {
     return (
-        <section className="benefit-section">
+        <section className="step-card">
             <h1>{props.number}</h1>
             <h2>{props.title}</h2>
             <p>{props.description}</p>
