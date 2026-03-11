0c30413ef479454cf677a470edd3966807cd0fa6
maha404 <maria_halvarsson@hotmail.se>
2026-03-11 20:22:59 +0100

    Add OurCommitment component and integrate it into AboutPage for commitment display

--- coffee-subscription-site/src/Pages/AboutPage.tsx
+++ coffee-subscription-site/src/Pages/AboutPage.tsx
@@ -2,7 +2,7 @@ import Header from '../Components/Header'
 import desktopHero from '../assets/about/desktop/image-hero-whitecup.jpg'
 import tabletHero from '../assets/about/tablet/image-hero-whitecup.jpg'
 import mobileHero from '../assets/about/mobile/image-hero-whitecup.jpg'
-import Modal from '../Components/Modal';
+import OurCommitment from '../Components/OurCommitmentSection/OurCommitment';
 
 export default function AboutPage() {
     return (
@@ -15,7 +15,7 @@ export default function AboutPage() {
             text="Coffeeroasters began its journey of exotic discovery in 1999, highlighting stories of coffee from around the world. We have since been dedicated to bring the perfect cup - from bean to brew - in every shipment."
             buttonVisible={false} 
             />
-            <Modal />
+            <OurCommitment />
         </>
     )
 }
