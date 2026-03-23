2757e31ac4ebd297e89dd4405482344cb0111c4b
maha404 <maria_halvarsson@hotmail.se>
2026-03-23 19:49:22 +0100

    Update App.css and NavigationMobile component for improved layout and routing

--- coffee-subscription-site/src/Components/Navigation/NavigationMobile.tsx
+++ coffee-subscription-site/src/Components/Navigation/NavigationMobile.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import logo from "../../assets/shared/desktop/logo.svg";
 import hamburgerIcon from "../../assets/shared/mobile/icon-hamburger.svg";
 import closeIcon from "../../assets/shared/mobile/icon-close.svg";
+import { Link } from "react-router";
 
 export default function NavigationMobile() {
     const [isOpen, setIsOpen] = useState(false);
@@ -24,9 +25,9 @@ export default function NavigationMobile() {
                 </button>
             </div>
             <ul className={isOpen ? "nav-menu nav-open" : "nav-menu nav-closed"}>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Create your plan</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/subscribe">Create your plan</Link></li>
             </ul>
         </nav>
     );
